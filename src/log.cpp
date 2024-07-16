3c797ec051bfa4c3747c1db1c89d8e96560eebf4
kfwu <kfwu0508@gmail.com>
2024-07-16 21:58:46 +0800

    feat: Add threadID in logging message

--- src/log.cpp
+++ src/log.cpp
@@ -19,10 +19,10 @@ void Log::init() {
     // 
     std::vector<spdlog::sink_ptr> logSinks;
     logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-    logSinks[0]->set_pattern("%^[%H:%M:%S][%n]: %v%$");
+    logSinks[0]->set_pattern("%^[%H:%M:%S][%t][%n]: %v%$");
 
     // 
-    s_logger = std::make_shared<spdlog::logger>("SERVER", logSinks.begin(), logSinks.end());   // TODO: use http?
+    s_logger = std::make_shared<spdlog::logger>("SERVER", logSinks.begin(), logSinks.end());
     spdlog::register_logger(s_logger);
     s_logger->set_level(spdlog::level::HTTP_LOG_LEVEL);
     s_logger->flush_on(spdlog::level::HTTP_LOG_LEVEL);
