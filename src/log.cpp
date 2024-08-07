629aff9d85860399d746f641e9a4d16a72662d04
kfwu <kfwu0508@gmail.com>
2024-08-07 18:37:34 +0800

    feat: Add file logger

--- src/log.cpp
+++ src/log.cpp
@@ -5,6 +5,7 @@
 
 #include <vector>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 #include "log.h"
 
@@ -18,8 +19,16 @@ std::shared_ptr<spdlog::logger> Log::s_logger;
 void Log::init() {
     // 
     std::vector<spdlog::sink_ptr> logSinks;
-    logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-    logSinks[0]->set_pattern("%^[%H:%M:%S][%t][%n]: %v%$");
+
+    // Console sink
+    auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    consoleSink->set_pattern("%^[%n][%H:%M:%S][%t]: %v%$");
+    logSinks.emplace_back(consoleSink);
+
+    // File sink
+    auto fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("server.log", true);
+    fileSink->set_pattern("[%n][%H:%M:%S][%t][%l]: %v");
+    logSinks.emplace_back(fileSink);
 
     // 
     s_logger = std::make_shared<spdlog::logger>("SERVER", logSinks.begin(), logSinks.end());
